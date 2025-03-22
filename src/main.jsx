a43337ef5ded114253367be13d8e7ab984a632ad
hacksaw2 <153176276+hacksaw2@users.noreply.github.com>
2025-03-22 10:20:56 +0530

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter,Route,createRoutesFromElements,BrowserRouter, RouterProvider } from 'react-router-dom'
-import Admin from './admin.jsx'
+import Admin from './Admin.jsx'
 import Home from './Home.jsx'
 import User from './User.jsx'
 
